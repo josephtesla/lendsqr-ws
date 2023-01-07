df872bd352f3d1c251429f7bdaf3fb0f4ab181df
Joseph <kstdeveloper@gmail.com>
2023-01-07 12:32:43 +0100

    Created wallet and transaction tables and migrations

--- src/db/migrations/00-create-user-table.ts
+++ src/db/migrations/00-create-user-table.ts
@@ -1,16 +1,26 @@
-import { Knex } from 'knex';
+import { Knex } from "knex";
+import { TableNames } from "../../constants";
+
+const { USERS } = TableNames
 
 export async function up(db: Knex): Promise<void> {
-	await db.schema.createTable('users', (table) => {
-		table.uuid('id').primary().defaultTo(db.raw('(UUID())'));
-    table.string('name', 255).notNullable();
-		table.string('email', 255).unique().notNullable();
-		table.string('password', 512).notNullable();
-		table.timestamp('createdAt', { useTz: false }).notNullable().defaultTo(db.fn.now());
-		table.timestamp('updatedAt', { useTz: false }).notNullable().defaultTo(db.fn.now());
-	});
+  await db.schema.createTable(USERS, (table) => {
+    table.increments("id").primary().unsigned();
+    table.uuid("uuid").defaultTo(db.raw("(UUID())"));
+    table.string("name", 255).notNullable();
+    table.string("email", 255).unique().notNullable();
+    table.string("password", 512).notNullable();
+    table
+      .timestamp("createdAt", { useTz: false })
+      .notNullable()
+      .defaultTo(db.fn.now());
+    table
+      .timestamp("updatedAt", { useTz: false })
+      .notNullable()
+      .defaultTo(db.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"));
+  });
 }
 
 export async function down(knex: Knex): Promise<void> {
-	await knex.schema.dropTable('users');
+  await knex.schema.dropTable(USERS);
 }
