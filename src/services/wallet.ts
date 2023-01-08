05c8156bfb9bbb878921c0e64f43c76621e6dc2c
Joseph <kstdeveloper@gmail.com>
2023-01-08 11:36:26 +0100

    Created services and endpoints to transfer money and get wallet transactions

--- src/services/wallet.ts
+++ src/services/wallet.ts
@@ -1,88 +1,196 @@
-import knexInstance from '../db'
-import { BadRequestError, NotFoundError } from '../helpers/error'
-import { TransactionRepository, WalletRepository } from '../repositories'
-import { PaymentService } from './payment'
+import knexInstance from "../db";
+import { BadRequestError, NotFoundError } from "../helpers/error";
+import { TransactionRepository, WalletRepository } from "../repositories";
+import { IBankAccountInfo } from "../types";
+import { PaymentService } from "./payment";
 
-interface IWalletService {}
+interface IWalletService {
+  createAccount: (userId: string) => Promise<any>;
+  getAccount: (userId: string) => Promise<any>;
+  fundAccount: (userId: string, amount: number) => Promise<any>;
+  withdraw: (
+    userId: string,
+    amount: number,
+    bankDetails: IBankAccountInfo
+  ) => Promise<any>;
+  transferToAccount: (
+    senderUserId: string,
+    toWalletId: string,
+    amount: number
+  ) => Promise<any>;
+}
 
 export class WalletService implements IWalletService {
-  private readonly transactionRepository: TransactionRepository
-  private readonly walletRepository: WalletRepository
+  private readonly transactionRepository: TransactionRepository;
+  private readonly walletRepository: WalletRepository;
 
-  constructor (
+  constructor(
     transactionRepository: TransactionRepository,
     walletRepository: WalletRepository
   ) {
-    this.transactionRepository = transactionRepository
-    this.walletRepository = walletRepository
+    this.transactionRepository = transactionRepository;
+    this.walletRepository = walletRepository;
   }
 
-  async createAccount (userId: string) {
-    const existingAccount = this.walletRepository.getOne({ userId })
+  async createAccount(userId: string) {
+    const existingAccount = await this.walletRepository.getOne({ userId });
     if (existingAccount !== null) {
-      throw new BadRequestError('Wallet account already exists for user')
+      throw new BadRequestError("Wallet account already exists for user");
     }
 
-    return await this.walletRepository.create({ userId })
+    return await this.walletRepository.create({ userId });
   }
 
-  async getAccount (userId: string) {
-    const account = this.walletRepository.getOne({ userId })
+  async getAccount(userId: string) {
+    const account = await this.walletRepository.getOne({ userId });
     if (account == null) {
-      throw new NotFoundError('Wallet account does not exist for user')
+      throw new NotFoundError("Wallet account does not exist for user");
     }
 
-    return await account
+    return account;
   }
 
-  async fundAccount (userId: string, amount: number) {
-    const account = await this.walletRepository.getOne({ userId })
-    if (account == null) {
-      throw new NotFoundError('Wallet account does not exist for user')
-    }
-
+  async fundAccount(userId: string, amount: number) {
     if (amount < 0) {
-      throw new BadRequestError('Invalid amount value')
+      throw new BadRequestError("Invalid amount value");
     }
 
-    await PaymentService.makePayment(userId, amount)
-    const newBalance = Number(account.balance) + amount
+    const account = await this.walletRepository.getOne({ userId });
+    if (account == null) {
+      throw new NotFoundError("Wallet account does not exist for user");
+    }
+
+    await PaymentService.makePayment(userId, amount);
+    const newBalance = Number(account.balance) + amount;
 
     await knexInstance.transaction(async (_trx) => {
-      await this.walletRepository.updateOne(account.id, { balance: newBalance }, _trx)
-      await this.transactionRepository.createDeposit({
-        userId,
-        walletId: account.id,
-        amount
-      }, _trx)
-    })
+      await this.walletRepository.updateOne(
+        account.id,
+        { balance: newBalance },
+        _trx
+      );
+      await this.transactionRepository.createDeposit(
+        {
+          userId,
+          walletId: account.id,
+          amount,
+        },
+        _trx
+      );
+    });
   }
 
-  async withdraw(userId: string, amount: number, bankDetails: any) {
-    const account = await this.walletRepository.getOne({ userId })
-    if (account == null) {
-      throw new NotFoundError('Wallet account does not exist for user')
-    }
-
+  async withdraw(
+    userId: string,
+    amount: number,
+    bankDetails: IBankAccountInfo
+  ) {
     if (amount < 0) {
-      throw new BadRequestError('Invalid amount value')
+      throw new BadRequestError("Invalid amount value");
     }
 
-    const accountBalance = Number(account.balance)
+    const account = await this.walletRepository.getOne({ userId });
+    if (account == null) {
+      throw new NotFoundError("Wallet account does not exist for user");
+    }
+
+    const accountBalance = Number(account.balance);
     if (amount > accountBalance) {
-      throw new BadRequestError('Insufficient funds in wallet to withdraw this amount')
+      throw new BadRequestError(
+        "Insufficient funds in wallet to withdraw this amount"
+      );
     }
 
-    await PaymentService.withdrawToBank(userId, amount, bankDetails)
+    await PaymentService.withdrawToBank(userId, amount, bankDetails);
+    const newBalance = accountBalance - amount;
+    const { bankName, bankAccountName, bankAccountNumber } = bankDetails;
 
-    const newBalance = accountBalance - amount
     await knexInstance.transaction(async (_trx) => {
-      await this.walletRepository.updateOne(account.id, { balance: newBalance }, _trx)
-      await this.transactionRepository.createWithdrawal({
-        userId,
-        walletId: account.id,
-        amount
-      }, _trx)
-    })
+      await this.walletRepository.updateOne(
+        account.id,
+        { balance: newBalance },
+        _trx
+      );
+      await this.transactionRepository.createWithdrawal(
+        {
+          userId,
+          walletId: account.id,
+          bankAccountName,
+          bankAccountNumber,
+          bankName,
+          amount,
+        },
+        _trx
+      );
+    });
+  }
+
+  async transferToAccount(userId: string, toWalletId: string, amount: number) {
+    if (amount < 0) {
+      throw new BadRequestError("Invalid amount value");
+    }
+
+    const senderAccount = await this.walletRepository.getOne({ userId });
+    if (senderAccount == null) {
+      throw new NotFoundError(
+        "Wallet account does not exist for the sending user"
+      );
+    }
+
+    if (senderAccount.id == toWalletId) {
+      throw new BadRequestError("Cannot transfer money to the same wallet");
+    }
+
+    const senderAccountBalance = Number(senderAccount.balance);
+    if (amount > senderAccountBalance) {
+      throw new BadRequestError(
+        "Insufficient funds in wallet to send this amount"
+      );
+    }
+
+    const toAccount = await this.walletRepository.getOneById(toWalletId);
+    if (toAccount == null) {
+      throw new NotFoundError("Recipient Wallet ID does not exist");
+    }
+
+    const newSenderAccountBalance = senderAccountBalance - amount;
+    const newRecipientAccountBalance = Number(toAccount.balance) + amount;
+
+    await knexInstance.transaction(async (_trx) => {
+      await this.walletRepository.updateOne(
+        senderAccount.id,
+        { balance: newSenderAccountBalance },
+        _trx
+      );
+
+      await this.walletRepository.updateOne(
+        toAccount.id,
+        { balance: newRecipientAccountBalance },
+        _trx
+      );
+
+      await this.transactionRepository.createTransfer(
+        {
+          toUserWalletId: toAccount.id,
+          fromUserWalletId: senderAccount.id,
+          toUserId: toAccount.userId,
+          fromUserId: userId,
+          amount,
+        },
+        _trx
+      );
+    });
+  }
+
+  async getTransactions(userId: string) {
+    const account = await this.walletRepository.getOne({ userId });
+    if (account == null) {
+      throw new BadRequestError("Wallet account does not exist for user");
+    }
+
+    return await this.transactionRepository.getWalletTransactions(
+      userId,
+      account.id
+    );
   }
 }
